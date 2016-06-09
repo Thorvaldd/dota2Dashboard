6d3e3614d14effb0a9cdcbf8c0cab209e40408da
Thorvaldd <1thorvald1@gmail.com>
2016-06-09 22:31:47 +0300

    Appharbor fix

--- Dota2Import/Settings.cs
+++ Dota2Import/Settings.cs
@@ -4,6 +4,9 @@ namespace Dota2Import
 {
     public static class Settings
     {
-        public static string UploadSmallImageDirectory => AppDomain.CurrentDomain.BaseDirectory + "small_hero_images\\";
+        public static string UploadSmallImageDirectory
+        {
+            get { return AppDomain.CurrentDomain.BaseDirectory + "small_hero_images\\"; }
+        } 
     }
 }
