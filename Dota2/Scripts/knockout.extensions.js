56b78b19d86cb55664205cb11f7c60f1f2cc7b28
alekseys <aleksey.sidorov@yanpix.com>
2016-06-10 12:17:03 +0300

    fixes

---
+++ Dota2/Scripts/knockout.extensions.js
@@ -0,0 +1,9 @@
+﻿ko.bindingHandlers.menu = {
+    init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+        $(element).preventDefault();
+	    $(element).closest('ul').find('li').removeClass('active');
+	    $(element).closest('li').addClass('active');
+	    
+        
+	}
+}
