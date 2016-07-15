98fbd51848d7c0949920cb1b433570dfe34d86be
alekseys <aleksey.sidorov@yanpix.com>
2016-07-15 17:58:13 +0300

    Charts js using

--- Dota2/Scripts/knockout-extensions.js
+++ Dota2/Scripts/knockout-extensions.js
@@ -59,44 +59,41 @@ ko.bindingHandlers.kdachart = {
 ko.bindingHandlers.swalChart = {
     init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
         var kda = ko.utils.unwrapObservable(valueAccessor());
-        var data = {
+
+        var lineChartData = {
             labels: ['K', 'D', 'A'],
-            series: [
-                [kda.Kills(), kda.Deaths(), kda.Assists()]
-            ],
-            low: 0,
-            showArea: false,
-            stackBars: true,
-            axisX: {
-                labelInterPolationFnc: function (value) {
-                    return value.split(/\s+/).map(function (word) {
-                        return word[0];
-                    }).join('');
-                }
-            }
+            datasets: [
+                {
+                    data: [kda.Kills(), kda.Deaths(), kda.Assists()]
+                },
+            ]
         }
-        
-       
         $(element)
             .mouseenter(function (e) {
-                var dynaminChartEl = "<div class='kda-" + e.pageX + "'> </div>";
+                var dynamicChartEl = "<canvas id='kda-"+ e.pageX + "'></canvas>";
                 console.log('X->' + e.pageX + ' Y->' + e.pageY);
                 swal({
-                    title: 'Chart',
+                    title: 'KDA stats',
                     confirmButtonText: 'X',
-                    text: "<div class='kda-"+e.pageX+"'>123</div>",
+                    text: dynamicChartEl,
                     html: true
                     
                 });
-                //$(element).append(dynaminChartEl);
-                if (dynaminChartEl.length == 0)
-                    return;
-
-                new Chartist.Line($('kda-'+e.pageX)[0], data);
+                var ctx = document.getElementById('kda-' + e.pageX).getContext('2d');
+                var lineChart = new Chart(ctx).Line(lineChartData, {
+                    pointDotRadius: 5,
+                    bezierCurve: false,
+                    scaleShowVerticalLines: false,
+                    options: {
+                        title: {
+                            xPadding: 8
+                        },
+                        fill: false
+                    },
+                });
             })
             .mouseleave(function(e) {
                 console.log('Leaving chart');
-                //$('.sa-confirm-button-container').find('button').click();
             });
     }
 }
